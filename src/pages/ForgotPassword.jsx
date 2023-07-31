0b208e3a0ffb7e62cc5a1999fedb3661cb7ee1ad
skematt <matthewyang2002@hotmail.com>
2023-07-30 23:39:23 -0700

    Added user and authentication features

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import {useState} from "react";
+import {Link} from "react-router-dom";
+import {getAuth, sendPasswordResetEmail} from "firebase/auth";
+import {toast} from "react-toastify";
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg';
 
 function ForgotPassword(props) {
+
+    const [email, setEmail] = useState('');
+    const onChange = (e) => {
+        setEmail(e.target.value);
+    }
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const auth = getAuth();
+            await sendPasswordResetEmail(auth, email)
+            toast.success('Reset email sent')
+        } catch (error) {
+            toast.error('Could not send reset email');
+        }
+    }
+
     return (
-        <div>
-            <h1>Forgot password</h1>
+        <div className={'pageContainer'}>
+            <header>
+                <p className="pageHeader">Forgot password</p>
+            </header>
+
+            <main>
+                <form onSubmit={onSubmit}>
+                    <input type="email"
+                        className="emailInput"
+                        placeholder="Email"
+                        id="email"
+                        value={email}
+                        onChange={onChange}
+                    />
+                    <Link className="forgotPasswordLink" to={'/sign-in'}>
+                        Return to Sign In
+                    </Link>
+
+                    <div className="signInBar">
+                        <div className="signInText">Send Reset Link</div>
+                        <button type="submit" className="signInButton">
+                            <ArrowRightIcon fill={'#ffffff'} width={'34px'} height={'34px'}/>
+                        </button>
+                    </div>
+                </form>
+            </main>
         </div>
     );
 }
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,100 @@
 import React from 'react';
+import {toast} from "react-toastify";
+import {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg';
+import VisibilityIcon from '../assets/svg/visibilityIcon.svg';
+import OAuth from "../components/OAuth";
+
 
 function SignIn(props) {
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+    const {email, password} = formData;
+
+    const navigate = useNavigate();
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value,
+        }))
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const auth = getAuth();
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+            if (userCredential.user) {
+                navigate('/');
+            }
+
+        } catch (error) {
+            toast.error('Bad user credentials');
+        }
+    }
+
     return (
-        <div>
-            <h1>Sign in</h1>
-        </div>
+        <>
+            <div className="pageContainer">
+                <header>
+                    <p className="pageHeader">
+                        Welcome Back!
+                    </p>
+                </header>
+
+                <form onSubmit={onSubmit}>
+                    <input type="email"
+                           className="emailInput"
+                           placeholder="Email"
+                           id="email"
+                           value={email}
+                           onChange={onChange}
+                    />
+                    
+                    <div className="passwordInputDiv">
+                        <input type={showPassword ? "text" : "password"}
+                               className="passwordInput"
+                               placeholder="Password"
+                               id="password"
+                               value={password}
+                               onChange={onChange}
+                        />
+
+                        <img
+                            src={VisibilityIcon}
+                            alt="show password"
+                            className="showPassword"
+                            fill={"#3c3c3c"}
+                            onClick={() => setShowPassword((prevState) => !prevState)}/>
+                    </div>
+
+                    <Link to='/forgot-password' className="forgotPasswordLink">
+                        Forgot password?
+                    </Link>
+
+                    <div className="signInBar">
+                        <p className="signInText">Sign In</p>
+                        <button className="signInButton">
+                            <ArrowRightIcon fill="#ffffff" width="34px" height="34px"/>
+                        </button>
+                    </div>
+                </form>
+
+                <OAuth />
+
+                <Link className="registerLink" to="/sign-up">
+                    Sign Up Instead
+                </Link>
+            </div>
+        </>
     );
 }
 
